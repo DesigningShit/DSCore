3ed0892590763b73c2b50b9b6f4729686ff6a38d
Matthew R. Puckett <mpuckett@designingshit.com>
2018-08-03 05:50:44 -0500

    backup

--- iot/serializers.py
+++ iot/serializers.py
@@ -1,37 +1,37 @@
 from rest_framework import viewsets
-from rest_framework import serializers
+from rest_framework import serializers as s
 from rest_framework import filters
 from .models import IOTChannelModel, IOTSensorModel, IOTSensorReadingModel
-import api.serializers
+# import api.serializers
 
 
 # Serializers are Here
-class IOTChannelModelSerializer(serializers.ModelSerializer):
+class IOTChannelModelSerializer(s.ModelSerializer):
     """
     IOT Channel Serializer
     """
-    owner_firstname = serializers.CharField(source='channelowner.first_name',read_only=True)
-    owner_lastname = serializers.CharField(source='channelowner.last_name',read_only=True)
+    owner_firstname = s.CharField(source='channelowner.first_name',read_only=True)
+    owner_lastname = s.CharField(source='channelowner.last_name',read_only=True)
     
     class Meta:
         model = IOTChannelModel
         fields = ('name','channelowner','owner_firstname','owner_lastname','channelid','created', 'modified')
         read_only_fields = ('channelid','created','modified','owner_firstname','owner_lastname')
 
-class IOTSensorModelSerializer(serializers.ModelSerializer):
+class IOTSensorModelSerializer(s.ModelSerializer):
     """
     IOT Sensor Serializer
     """
     channel = IOTChannelModel.objects.all()
-    owner = serializers.CharField(source='channel.channelowner.userkey',read_only=True)
-    channelname = serializers.CharField(source='channel.name',read_only=True)
+    owner = s.CharField(source='channel.channelowner.userkey',read_only=True)
+    channelname = s.CharField(source='channel.name',read_only=True)
 
     class Meta:
         model = IOTSensorModel
         fields = ('name' , 'channel' ,'channelname','context' , 'sensorid' , 'owner' , 'created' , 'modified')
         read_only_fields = ('sensorid' , 'created' , 'owner', 'modified')
 
-class IOTSensorReadingModelSerializer(serializers.ModelSerializer):
+class IOTSensorReadingModelSerializer(s.ModelSerializer):
     """
     IOT Sensor Data Serializer
     """
