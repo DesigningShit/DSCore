249db613dbe624b0ecaf8035df5bb0a3c3b26daf
Matthew R. Puckett <mpuckett@designingshit.com>
2018-08-06 01:12:10 -0500

    Added Coverage for Autoamted Unit Testing

--- dscore/settings.py
+++ dscore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'django_coverage',
     # Add additional extensions
     'rest_framework',
     'webpack_loader',
